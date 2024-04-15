09160569c8a80867f1fe0888e52686aaf18e7bf6
moodiesam <sampmcampbell@gmail.com>
2024-04-15 17:31:26 -0400

    create full CRUD functionality with database

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,17 +10,40 @@ exports.create_post = asyncHandler(async (req, res, next) => {
     if (!req.body.title) {
         res.status(400).json({ message: "please add a title field" })
     }
-    res.status(200).json({ message: `CREATE post` });
+
+    const post = await Post.create({
+        title: req.body.title,
+        text: req.body.text,
+    })
+    res.status(200).json(post);
 })
 
 exports.read_post = asyncHandler(async (req, res, next) => {
-    res.status(200).json({ message: `READ posts` });
+    const posts = await Post.find()
+
+    res.status(200).json({ message: `READ posts`, posts });
 })
 
 exports.update_post = asyncHandler(async (req, res, next) => {
-    res.status(200).json({ message: `UPDATE post ${req.params.id}` });
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+        res.status(400).json({ message: "post not found" })
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedPost);
 })
 
 exports.delete_post = asyncHandler(async (req, res, next) => {
-    res.status(200).json({ message: `DELETE post ${req.params.id}` });
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+        res.status(400).json({ message: "post not found" })
+    }
+
+    await Post.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({ message: `Post: ${post.title}... has been deleted` });
 })
